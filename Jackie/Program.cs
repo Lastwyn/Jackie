cc5bf157439e13f5037b5b4065f2e56a40f6ee10
Dávid Sőregi <davesoregi@gmail.com>
2022-10-11 20:35:48 +0200

    Add project files.

---
+++ Jackie/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Jackie
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            List<Stat> adatok = new List<Stat>();
+            
+            using (StreamReader file = new StreamReader("jackie.txt"))   //2. feladat Beolvasás
+            { 
+                file.ReadLine();
+                while (!file.EndOfStream)
+                {
+                    adatok.Add(new Stat(file.ReadLine()));
+                }
+
+            }
+            //3. feladat hűny adatsor van az állományban
+            Console.WriteLine($"3. feladat: {adatok.Count}");
+
+            //4. feladat melyik évben indult el a legtöbb versenyen
+            Console.WriteLine($"4. feladat: {adatok.First(x => x.Versenyek == adatok.Max(y => y.Versenyek)).Ev}");
+
+            //5. feladat Jackie Stewart számára melyik évtized mennyire volt sikeres
+            Console.WriteLine($"5. feladat:\n\r\t70-as évek {adatok.Where(x => x.Ev >= 1970 && x.Ev < 1980).Sum(y => y.Gyozelmek)} megnyert verseny\n\r\t60-as évek {adatok.Where(x => x.Ev >= 1960 && x.Ev < 1970).Sum(y => y.Gyozelmek)}" + " megnyert verseny");
+
+            
+            using (StreamWriter html = new StreamWriter("jackie.html", false))
+            {
+                html.WriteLine("<!DOCTYPE html>");
+                html.WriteLine("<html lang=\"HU\">");
+                html.WriteLine("\t<head><style>td{border: solid 1px #000;}</style></head>");
+                html.WriteLine("\t<body>");
+                html.WriteLine("\t\t<h1>Jackie Stewart</h1>");
+                html.WriteLine("\t\t<table border=\"1px\">");
+                html.WriteLine("\t\t\t<thead><tr><th>Év</th><th>Versenyek száma</th><th>Győzelmek száma</th></tr></thead>");            //6. feladat a html oldal elkészitése
+                html.WriteLine("\t\t\t<tbody>");
+                foreach (Stat item in adatok.OrderByDescending(x => x.Ev))
+                {
+                    html.WriteLine($"\t\t\t\t<tr><td>{item.Ev}</td><td>{item.Versenyek}</td><td>{item.Gyozelmek}</td></tr>");
+                }
+                html.WriteLine("\t\t\t</tbody>");
+                html.WriteLine("\t\t</table>");
+                html.WriteLine("\t</body>");
+                html.WriteLine("</html>");
+            }
+            Console.WriteLine("6. feladat: jackie.html");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Jackie/Stat.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jackie
+{
+    class Stat
+    {
+        int ev, versenyek, gyozelmek, podiumok, elsohely, leggyorsabb;
+
+        public int Ev { get => ev; set => ev = value; }
+        public int Versenyek { get => versenyek; set => versenyek = value; }
+        public int Gyozelmek { get => gyozelmek; set => gyozelmek = value; }
+        public int Podiumok { get => podiumok; set => podiumok = value; }
+        public int Elsohely { get => elsohely; set => elsohely = value; }
+        public int Leggyorsabb { get => leggyorsabb; set => leggyorsabb = value; }
+
+        public Stat(int ev, int versenyek, int gyozelmek, int podiumok, int elsohely, int leggyorsabb)
+        {
+            Ev = ev;
+            Versenyek = versenyek;
+            Gyozelmek = gyozelmek;
+            Podiumok = podiumok;
+            Elsohely = elsohely;
+            Leggyorsabb = leggyorsabb;
+        }
+
+        public Stat(string adatsor)
+        {
+            string[] adatok = adatsor.Split('\t');
+            Ev = int.Parse(adatok[0]);
+            Versenyek = int.Parse(adatok[1]);
+            Gyozelmek = int.Parse(adatok[2]);
+            Podiumok = int.Parse(adatok[3]);
+            Elsohely = int.Parse(adatok[4]);
+            Leggyorsabb = int.Parse(adatok[5]);
+        }
+    }
+}
